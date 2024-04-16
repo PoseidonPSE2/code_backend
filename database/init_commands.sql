06a7557c1d628d336bcd9c79edf22b2bc90ef5e5
Alejandro Restrepo Klinge <56825548+mrrestre@users.noreply.github.com>
2024-04-16 10:26:56 +0200

    start working on the database

---
+++ database/init_commands.sql
@@ -0,0 +1,61 @@
+create database poseidon encoding 'UTF8';
+
+-- Use Database
+\c poseidon;
+
+CREATE TABLE station (
+    id          SERIAL PRIMARY KEY,
+    created_at  TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    latitude    DOUBLE PRECISION CHECK (latitude >= -90 AND latitude <= 90),
+    longitude   DOUBLE PRECISION CHECK (longitude >= -180 AND longitude <= 180),
+    title       TEXT,
+    description TEXT,
+    open_times  TEXT
+);
+
+CREATE TABLE "user" (
+    id          SERIAL PRIMARY KEY,
+    created_at  TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    first_name  TEXT,
+    last_name   TEXT
+);
+
+CREATE TABLE refill_event (
+    id          SERIAL PRIMARY KEY,
+    created_at  TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    milliliter  NUMERIC,
+    user_id     INT REFERENCES "user" (id),
+    station_id  INT REFERENCES station (id)
+);
+
+-- Show Tables in Postgres 
+\dt; 
+
+-- Generate test data for the user table
+INSERT INTO "user" (first_name, last_name)
+VALUES
+    ('John', 'Doe'),
+    ('Jane', 'Smith'),
+    ('Michael', 'Johnson');
+
+-- Generate test data for the station table (assuming latitude and longitude for Kaiserslautern)
+INSERT INTO station (latitude, longitude, title, description, open_times)
+VALUES
+    (49.4444, 7.7689, 'Station 1', 'Description for Station 1', 'Mon-Fri: 8am-6pm, Sat: 9am-3pm'),
+    (49.4331, 7.7746, 'Station 2', 'Description for Station 2', 'Mon-Fri: 9am-5pm, Sat: 10am-2pm'),
+    (49.4447, 7.7693, 'Station 3', 'Description for Station 3', 'Mon-Fri: 7:30am-7pm, Sat: 8am-4pm');
+
+-- Generate test data for the refill_event table (run multiple times for it to work)
+INSERT INTO refill_event (created_at, milliliter, user_id, station_id)
+SELECT
+    NOW() - INTERVAL '1' DAY * (random() * 30) AS created_at,
+    CASE floor(random() * 5)
+        WHEN 0 THEN 1000
+        WHEN 1 THEN 750
+        WHEN 2 THEN 600
+        WHEN 3 THEN 500
+        ELSE 200
+    END AS milliliter,
+    (SELECT id FROM "user" OFFSET floor(RANDOM() * (SELECT COUNT(*) FROM "user")) LIMIT 1) user_id,
+    (SELECT id FROM station OFFSET floor(RANDOM() * (SELECT COUNT(*) FROM station)) LIMIT 1) station_id
+FROM generate_series(1, 5) s;
